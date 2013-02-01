0f63fd70bfcb6c1736f430b106d237d7ce9181d0
developer <developer@DEV-002.(none)>
2013-02-01 16:56:16 +0000

    added first round trip

--- test/ios-test.js
+++ test/ios-test.js
@@ -2,7 +2,8 @@ var ios = require('../lib/ios.js');
 
 describe('with valid config', function() { 
   var config = JSON.parse(process.env.IOS_MOCK_CONFIG);
-  
+  this.timeout(200000);
+
   it('calling commission should not throw', function(done) {
     ios.commission(config, done);
   })
